d116cc5c8502c09f8597ba716735d9319ea77410
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-03-07 12:12:02 +0530

    Added Header.js and Footer.js files and some dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Seller_addItems from './Components/Seller_addItems';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 function App() {
   return (
 
   <div>
 
-    <Seller_addItems/>
+    <Header/>
+    <Footer/>
+    
 
   </div>
    
---
+++ src/Components/Header.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+//import './Gama_CSS.css'
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBContainer,
+  MDBIcon,
+  MDBCollapse,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+
+export default function Header() {
+
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <header>
+      <MDBNavbar expand='lg' light bgColor='white'>
+        <MDBContainer fluid>
+          <MDBNavbarToggler
+            onClick={() => setShowBasic(!showBasic)}
+            aria-controls='navbarExample01'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <MDBIcon fas icon='bars' />
+          </MDBNavbarToggler>
+          <MDBCollapse navbar show={showBasic}>
+            <MDBNavbarNav right className='mb-2 mb-lg-0'>
+              <MDBNavbarItem active>
+                <MDBNavbarLink aria-current='page' href='#'>
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Our Products</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Delivery Services</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>About Us</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+
+      <div
+        className='p-5 text-center bg-image'
+        style={{ backgroundImage: "url('https://img.freepik.com/free-photo/chyavanprash-chyawanprash-is-widely-consumed-india-as-dietary-ayurvedic-supplement_466689-92032.jpg?t=st=1678167132~exp=1678167732~hmac=b971e5d3c6bec0d6541ec887cdbed45bc0c8982d0976cc7d9df6bf07dba68a86')", height: '400px' }}
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'><b>REVONTA</b></h1>
+              <h4 className='mb-3' class="caption_text">"The Pride Of Ayurvedic!"</h4>
+              <MDBBtn tag="a" outline size="lg">
+                Get more..
+              </MDBBtn>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
