b30b1ec14b335d67dee06053af40a538c6c2113e
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-03-07 08:16:40 +0530

    Added all basic ReactJs Files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Seller_addItems from './Components/Seller_addItems';
+
+function App() {
+  return (
+
+  <div>
+
+    <Seller_addItems/>
+
+  </div>
+   
+    
+  )
+}
+
+export default App;
---
+++ src/Components/Seller_addItems.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Seller_addItems extends Component {
+  render() {
+    return (
+      <div>Add items</div>
+    )
+  }
+}
